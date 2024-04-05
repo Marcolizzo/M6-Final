41a2fb1d29f6666a5b674bad2ae4e430043716d2
Marco Santoro <msantor@hotmail.it>
2024-04-05 16:47:24 +0200

    Install cors
    installazione cors e aggiornamento del main.js

--- be/main.js
+++ be/main.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/dbConfig');
+const cors = require('cors');
 //import delle routes
 const authorsRoute = require('./routes/authors');
 // creazione porta (questa porta non deve andare in conflitto con quella del fe)
@@ -9,6 +10,7 @@ const PORT = 3030;
 const app = express();
 
 //middleware
+app.use(cors())
 app.use(express.json());
 app.use('/', authorsRoute);
 
